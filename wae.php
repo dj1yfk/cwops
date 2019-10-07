c6d4acd9d79ec9632c58a7e0c3e954e08dc34ecd
Fabian Kurz <fabian@fkurz.net>
2019-10-07 19:25:14 +0200

    add missing states/wae lists

---
+++ wae.php
@@ -0,0 +1,11 @@
+<?
+$waes = array(
+    "IV" => "ITU Vienna, 4U1",
+    "AI" => "African Italy, IG9",
+    "SY" => "Sicily, IT9",
+    "BI" => "Bear Island, JW/B",
+    "SI" => "Shetland Islands, GM/S",
+    "KO" => "Kosovo, Z6",
+    "ET" => "European Turkey, TA1"
+);
+?>
