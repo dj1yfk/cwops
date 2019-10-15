f5247ecd339b028a37682eea0427ff5b9ba4e203
Fabian Kurz <fabian@fkurz.net>
2019-10-15 22:38:25 +0200

    cwops_log: Add id column

--- db/cwops_log.sql
+++ db/cwops_log.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `cwops_log` (
+  `id` bigint(5) NOT NULL auto_increment,
   `mycall` varchar(64) NOT NULL default '',
   `date` date NOT NULL,
   `year` int NOT NULL default 0,
@@ -9,6 +10,7 @@ CREATE TABLE `cwops_log` (
   `wae` varchar(2) NOT NULL default '',
   `waz` int NOT NULL default 0,
   `was` varchar(2) NOT NULL default '',
+  PRIMARY KEY (`ID`),
   KEY (`mycall`),
   KEY(`nr`)
-);
+) AUTO_INCREMENT = 1;
